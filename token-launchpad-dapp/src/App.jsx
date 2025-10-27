6a6fd2d1b94e3cc23d34d0f6bbcd730f76369626
Rahul <rahul.mymail1@gmail.com>
2025-10-27 19:26:23 +0530

    feat: mint account + metadata

---
+++ token-launchpad-dapp/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+// import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { TokenLaunchpad } from './components/AttachMetaData'
+
+
+// wallet adapter imports
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+function App() {
+  return (
+    <div style={{width: "100vw"}}>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  )
+}
+
+export default App
